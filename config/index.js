b9a5debdd0109a9caea043199a2006c3e02343b9
gengen517 <49915479+gengen517@users.noreply.github.com>
2022-09-06 10:18:12 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ogncX6hvqAdXuInwdoTm-c1esTy8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "feRLtVoH66ZxM36pVd4_HDXq7r_YlGnaMsdodi2v1oA",
+      useTemplateId: "RQ_Yp4HBZ0YGEa9g7hOmJ0WZ_g7F-7_IY8rpi_fd31g",
       // 所在省份
       province: "陕西",
       // 所在城市
@@ -51,7 +51,7 @@ export const config = {
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
   // CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
-  CALLBACK_TEMPLATE_ID: "feRLtVoH66ZxM36pVd4_HDXq7r_YlGnaMsdodi2v1oA",
+  CALLBACK_TEMPLATE_ID: "RQ_Yp4HBZ0YGEa9g7hOmJ0WZ_g7F-7_IY8rpi_fd31g",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
